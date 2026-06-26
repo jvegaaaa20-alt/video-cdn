39efda244bb1c35ec4b13840cf9d58e3ecefd719
jvegaaaa20-alt <jvegaaaa20@gmail.com>
2026-06-27 00:51:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from flask import Flask, send_file, request, Response
+from flask import Flask, request, Response
 import os
 
 app = Flask(__name__)
 
+VIDEO_FILE = 'IMG_5129.MOV'
+
 @app.route('/video.mp4')
 def video():
-    return send_file(
-        'IMG_5129.MOV',
-        mimetype='video/mp4',
-        conditional=True
-    )
+    file_size = os.path.getsize(VIDEO_FILE)
+    range_header = request.headers.get('Range', None)
+
+    if range_header:
+        byte_start, byte_end = 0, None
+        match = range_header.replace('bytes=', '').split('-')
+        byte_start = int(match[0])
+        byte_end = int(match[1]) if match[1] else file_size - 1
+
+        length = byte_end - byte_start + 1
+
+        with open(VIDEO_FILE, 'rb') as f:
+            f.seek(byte_start)
+            data = f.read(length)
+
+        rv = Response(data, 206, mimetype='video/mp4', direct_passthrough=True)
+        rv.headers.add('Content-Range', f'bytes {byte_start}-{byte_end}/{file_size}')
+        rv.headers.add('Accept-Ranges', 'bytes')
+        rv.headers.add('Content-Length', str(length))
+        return rv
+
+    with open(VIDEO_FILE, 'rb') as f:
+        data = f.read()
+
+    rv = Response(data, 200, mimetype='video/mp4')
+    rv.headers.add('Accept-Ranges', 'bytes')
+    rv.headers.add('Content-Length', str(file_size))
+    return rv
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
