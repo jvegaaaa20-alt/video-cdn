9f02d605f2566c6eb7c4a454865e243e0f4e58a8
jvegaaaa20-alt <jvegaaaa20@gmail.com>
2026-06-27 00:44:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
-from flask import Flask, send_file, Response
+from flask import Flask, send_file
 import os
 
 app = Flask(__name__)
 
 @app.route('/video.mp4')
 def video():
-    return send_file('video.mp4', mimetype='video/mp4')
+    return send_file('IMG_5129.MOV', mimetype='video/mp4')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
